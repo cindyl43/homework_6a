4abdd8290a709e3a136e521e9c468f8969cea480
cindyl43 <xinyangl@andrew.cmu.edu>
2021-10-24 18:27:06 -0400

    cart page update

---
+++ main.js
@@ -0,0 +1,21 @@
+/*** Object Constructors ***/
+
+function Green (color, filling) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "Green Chinese painting picture with mountain and water";
+  this.image = "larpic.jpg";
+}
+
+function Pink (color, filling) {
+  this.color = color;
+  this.filling = filling;
+  this.image_alt = "Pink Chinese painting picture with cherry blossom flower";
+  this.image = "pink.jpg";
+}
+
+function addItemToCart (name, color, filling, quantity, imgaeSrc) {
+  var cartRow = document.createElement('div')
+  vaar carItems = document.getElementByClassName('cart-items')[0]
+  cartItems.append(cartRow)
+}
